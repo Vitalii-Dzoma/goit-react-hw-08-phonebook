10368abef53291d89d1fc4404d4ac7135c2c818a
Vitalii-Dzoma <vitoos900@gmail.com>
2022-05-05 19:12:02 +0300

    bafore changes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,8 +9,16 @@ import { ContactList } from './ContactlList/ContactList';
 import { CreateContactPage } from './CreateContact/CreateContact';
 import Filter from './Filter/Filter';
 import { useGetContactsQuery } from 'redux/contacts';
+import PrivateRoute from '../components/PrivateRoute/PrivateRoute';
+import PublicRoute from '../components/PublicRoute/PublicRoute';
 
 export const App = () => {
+  const dispatch = useDispatch();
+  const isFetchingCurrentUser = useSelector(authSelectors.getIsFetchingCurrent);
+
+  useEffect(() => {
+    dispatch(authOperations.fetchCurrentUser());
+  }, [dispatch]);
   return (
     <div
       style={{
@@ -23,9 +31,21 @@ export const App = () => {
       <Provider store={store}>
         <Suspense>
           <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="contacts" element={<App1 />}></Route>
-            <Route path="contacts/create" element={<CreateContactPage />} />
+            <PublicRoute exact path="/">
+              <HomePage />
+            </PublicRoute>
+            <PublicRoute exact path="/register" restricted>
+              <RegisterView />
+            </PublicRoute>
+            <PublicRoute exact path="/login" redirectTo="/contacts" restricted>
+              <LoginView />
+            </PublicRoute>
+            <PrivateRoute path="/contacts" redirectTo="/login">
+              <App1 />
+            </PrivateRoute>
+            <PrivateRoute path="/contacts/create" redirectTo="/login">
+              <CreateContactPage />
+            </PrivateRoute>
             <Route path="*" element={<HomePage />} />
           </Routes>
         </Suspense>
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,18 +1,43 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { authReducer } from './auth';
 import { contactApi } from './contacts';
 import { setupListeners } from '@reduxjs/toolkit/dist/query';
 import { itemsReducer, filterSlice } from './../redux/itemsSlice';
+
+const middleware = [
+  ...getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
+];
+
+const authPersistConfig = {
+  key: 'auth',
+  storage,
+  whitelist: ['token'],
+};
 
 export const store = configureStore({
   reducer: {
     items: itemsReducer,
     filter: filterSlice.reducer,
     [contactApi.reducerPath]: contactApi.reducer,
+    auth: persistReducer(authPersistConfig, authReducer),
   },
-  middleware: getDefaultMiddleware => [
-    ...getDefaultMiddleware(),
-    contactApi.middleware,
-  ],
+  middleware,
+  devTools: process.env.NODE_ENV === 'development',
 });
-
+export const persistor = persistStore(store);
 setupListeners(store.dispatch);
